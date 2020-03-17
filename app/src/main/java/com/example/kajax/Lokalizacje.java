aaf551f64b9584bfd1847639413534729df2ef73
kinga <kinga081@gmail.com>
2020-03-17 01:49:27 +0100

    baza danych

---
+++ app/src/main/java/com/example/kajax/Lokalizacje.java
@@ -0,0 +1,57 @@
+package com.example.kajax;
+
+class Lokalizacje {
+/*
+    "localid": "-LTdVr2MaVHfqOlKxkzK-",
+            "name": "Spływy Kajakowe po rzekach Roztocza - \"U Rudego\"",
+            "adress": "Obrocz 116, 22-470 Obrocz",
+            "h_open": "cały tydzień 09:00–18:00",
+            "tel": 885604286,
+            "web": "splywy-roztocze.pl",
+            "Latitude": 50.6027829,
+            "longitude": 23.0264988
+
+ */
+    String localid;
+    String name;
+    String adress;
+    String h_open;
+    float tel;
+    String web;
+    float Latitude;
+    float longitude;
+
+    Lokalizacje(){}
+
+    public String getLocalid() {
+        return localid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public String getH_open() {
+        return h_open;
+    }
+
+    public float getTel() {
+        return tel;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public float getLatitude() {
+        return Latitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+}
--- app/src/main/java/com/example/kajax/MapsActivity.java
+++ app/src/main/java/com/example/kajax/MapsActivity.java
@@ -22,58 +22,77 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback,GoogleMap.OnMyLocationButtonClickListener,
-        GoogleMap.OnMyLocationClickListener {
+        GoogleMap.OnMyLocationClickListener,GoogleMap.OnMarkerClickListener, GoogleMap.OnMapClickListener{
 
     private GoogleMap mMap;
 
     public static final String TAG = "GoogleActivity";
-    private static final int RECORD_REQUEST_CODE =101 ;
+    private static final int RECORD_REQUEST_CODE = 101;
+    private DatabaseReference mUsers;
+    private HashMap<String, String> meMap;
+    private Marker marker;
+    private FloatingActionButton info;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_main);
+        setContentView( R.layout.activity_main );
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById( R.id.map );
         mapFragment.getMapAsync( this );
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        Toolbar toolbar = (Toolbar) findViewById( R.id.toolbar );
+        setSupportActionBar( toolbar );
         getSupportActionBar().setTitle( "Kajax" );
-        toolbar.setTitleTextColor( Color.WHITE);
-       // toolbar.setLogo( R.drawable.kayak );
+        toolbar.setTitleTextColor( Color.WHITE );
+        // toolbar.setLogo( R.drawable.kayak );
 
         //getSupportActionBar().setDisplayShowHomeEnabled(true);
 
-        int permissionCheck = ContextCompat.checkSelfPermission(this,//pozwolenie
-                android.Manifest.permission.ACCESS_FINE_LOCATION);
-        int permissionCheck2 = ContextCompat.checkSelfPermission(this,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION);
-        int permissionCheck3 = ContextCompat.checkSelfPermission(this,
-                android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        int permissionCheck = ContextCompat.checkSelfPermission( this,//pozwolenie
+                android.Manifest.permission.ACCESS_FINE_LOCATION );
+        int permissionCheck2 = ContextCompat.checkSelfPermission( this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION );
+        int permissionCheck3 = ContextCompat.checkSelfPermission( this,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE );
 
         if (permissionCheck != PackageManager.PERMISSION_GRANTED || permissionCheck2 !=
                 PackageManager.PERMISSION_GRANTED || permissionCheck3 !=
                 PackageManager.PERMISSION_GRANTED) {
-            Log.i(TAG, "Permission to record denied");
+            Log.i( TAG, "Permission to record denied" );
             makeRequest();
         }
 
-        FloatingActionButton fab = findViewById( R.id.fab );
-        fab.setOnClickListener( new View.OnClickListener() {
+        info = (FloatingActionButton) findViewById(R.id.info);
+
+        info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make( view, "Replace with your own action", Snackbar.LENGTH_LONG )
-                        .setAction( "Action", null ).show();
+                android.app.AlertDialog.Builder builder =
+                        new android.app.AlertDialog.Builder(MapsActivity.this);
+                builder.setTitle("title");
+                builder.setMessage("value");
+                builder.setPositiveButton("Zamknij", null);
+                builder.show();
+
             }
-        } );
+        });
     }
 
 
@@ -88,55 +107,111 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.domyslny) {
-            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            mMap.setMapType( GoogleMap.MAP_TYPE_NORMAL );
             return true;
-        }else if(id == R.id.satelita) {
-            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        } else if (id == R.id.satelita) {
+            mMap.setMapType( GoogleMap.MAP_TYPE_SATELLITE );
             return true;
-        }else if(id == R.id.teren) {
-            mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+        } else if (id == R.id.teren) {
+            mMap.setMapType( GoogleMap.MAP_TYPE_TERRAIN );
+            return true;
+        }else if (id == R.id.kajaki) {
+            dodajMarker( "Kayaks",true );
+            return true;
+        }else if (id == R.id.jedzenie) {
+            dodajMarker( "Gastronomy",false );
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected( item );
     }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission( this, Manifest.permission.ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission( this, Manifest.permission.ACCESS_COARSE_LOCATION ) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
 
             return;
         }
         mMap = googleMap;
 
-        mMap.setMyLocationEnabled(true);
-        mMap.setOnMyLocationButtonClickListener(this);
-        mMap.setOnMyLocationClickListener(this);
+        mMap.setMyLocationEnabled( true );
+        mMap.setOnMyLocationButtonClickListener( this );
+        mMap.setOnMyLocationClickListener( this );
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(50.602132, 23.031859)));
-        mMap.animateCamera(CameraUpdateFactory.zoomTo(18));
+        mMap.moveCamera( CameraUpdateFactory.newLatLng( new LatLng( 50.6027829, 23.0264988 ) ) );
+        mMap.animateCamera( CameraUpdateFactory.zoomTo( 15 ) );
     }
 
 
     public void onMyLocationClick(@NonNull Location location) {
-        Toast.makeText(this, "Moja lokalizacja: " + location.getLatitude()+","+location.getLongitude(), Toast.LENGTH_LONG).show();
+        Toast.makeText( this, "Moja lokalizacja: " + location.getLatitude() + "," + location.getLongitude(), Toast.LENGTH_LONG ).show();
         //info.setVisibility(View.INVISIBLE);
     }
 
 
-
     public boolean onMyLocationButtonClick() {
-        Toast.makeText(this, "Moja lokalizacja", Toast.LENGTH_SHORT).show();
+        Toast.makeText( this, "Moja lokalizacja", Toast.LENGTH_SHORT ).show();
         //info.setVisibility(View.INVISIBLE);
         return false;
     }
 
     protected void makeRequest() {
-        ActivityCompat.requestPermissions(this,
+        ActivityCompat.requestPermissions( this,
                 new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,
                         android.Manifest.permission.ACCESS_COARSE_LOCATION,
                         Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 },
-                RECORD_REQUEST_CODE);
+                RECORD_REQUEST_CODE );
+    }
+
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        //title = marker.getTitle();
+        //info.setVisibility(View.VISIBLE);
+        //value = meMap.get(title);
+        return false;
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        //info.setVisibility(View.INVISIBLE);
+    }
+
+    public void dodajMarker(String kategoria, final boolean kolor) {// true== kajaki
+        info.setVisibility( View.VISIBLE );
+
+        mUsers = FirebaseDatabase.getInstance().getReference( kategoria );
+        mUsers.push().setValue( marker );
+        mMap.clear();
+       // meMap = new HashMap<String, String>();
+        mUsers.addListenerForSingleValueEvent( new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot s : dataSnapshot.getChildren()) {
+                    Lokalizacje user = s.getValue( Lokalizacje.class );
+                    LatLng location = new LatLng( Double.parseDouble( String.valueOf( user.getLatitude() ) ), Double.parseDouble( String.valueOf( user.getLongitude() ) ) );
+                   // meMap.put( user.getNazwa(), user.getOpis() );
+                    if (kolor) {
+                        mMap.addMarker( new MarkerOptions()
+                                .position( location )
+                                .title( user.getName() )
+                                .icon( BitmapDescriptorFactory.defaultMarker( BitmapDescriptorFactory.HUE_ROSE ) )
+                        );
+                    } else {
+                        mMap.addMarker( new MarkerOptions()
+                                .position( location )
+                                .title( user.getName() )
+                                .icon( BitmapDescriptorFactory.defaultMarker( BitmapDescriptorFactory.HUE_GREEN ) )
+                        );
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        } );
     }
 }
